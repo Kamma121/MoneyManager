d5bbf5664426eabe1fea97092a0304fc53bdc918
Kamil <KamilSodo132@gmail.com>
2023-12-19 11:23:16 +0100

    init

---
+++ server/src/main/java/com/moneymanager/backend/MoneymanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moneymanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneymanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneymanagerBackendApplication.class, args);
+	}
+
+}
