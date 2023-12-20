dd679323e8844742c5f2efd788e4cf10f923bbbc
Kamil <KamilSodo132@gmail.com>
2023-12-20 10:45:48 +0100

    Created component structure, added carousel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -8,12 +8,20 @@ import { AppRoutingModule } from './app-routing.module';
 import {RouterOutlet} from "@angular/router";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NavigationBarComponent } from './home-page/navigation-bar/navigation-bar.component';
+import { SignInModalComponent } from './home-page/sign-in-modal/sign-in-modal.component';
+import { SignUpModalComponent } from './home-page/sign-up-modal/sign-up-modal.component';
+import { CarouselComponent } from './home-page/carousel/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavigationBarComponent,
+    SignInModalComponent,
+    SignUpModalComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
