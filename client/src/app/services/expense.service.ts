729b7a78c8feea778ff1ddc351183a161cb7ddb4
Kamil <KamilSodo132@gmail.com>
2024-01-01 16:39:10 +0100

    Fixed expense service

--- client/src/app/services/expense.service.ts
+++ client/src/app/services/expense.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {Expense} from "../shared/Expense";
 
@@ -7,32 +7,43 @@ import {Expense} from "../shared/Expense";
 })
 export class ExpenseService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
+
   httpOptions = {
     headers: new HttpHeaders({
       'Authorization': 'Bearer ' + localStorage.getItem('token')
     })
   };
 
-  getAllExpenses(){
-    return this.http.get<Expense[]>('http://localhost:8080/api/expenses',this.httpOptions);
+  getAllExpenses() {
+    return this.http.get<Expense[]>('http://localhost:8080/api/expenses', this.httpOptions);
   }
-  addExpenses(expenses:Expense[]){
-    return this.http.post('http://localhost:8080/api/expense/add',expenses,this.httpOptions);
+
+  addExpenses(expenses: Expense[]) {
+    return this.http.post('http://localhost:8080/api/expense/add', expenses, this.httpOptions);
   }
 
   deleteExpense(id: number | null) {
-    if(id === null){
+    if (id === null) {
       console.log('Id not found');
       return null;
-    }else{
+    } else {
       return this.http.delete(`http://localhost:8080/api/expense/${id}`, {
         ...this.httpOptions,
         responseType: 'text'
       });
     }
   }
-  updateExpense(expense:Expense){
-    return this.http.put(`http://localhost:8080/api/expense/${expense.id}`,expense,this.httpOptions)
+
+  updateExpense(expense: Expense) {
+    const currExpense: Expense = {
+      id: expense.id,
+      date: expense.date,
+      category: expense.category,
+      amount: expense.amount,
+      message: expense.message
+    }
+    return this.http.put(`http://localhost:8080/api/expense/${expense.id}`, currExpense, this.httpOptions)
   }
 }
